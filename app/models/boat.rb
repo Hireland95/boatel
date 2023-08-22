6deeec80b049b008817241dd5e897a4bf914720e
Henry Ireland <henryireland95@gmail.com>
2023-08-22 14:12:36 +0100

    Added create action to controller and cloudinary to new action

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -1,6 +1,7 @@
 class Boat < ApplicationRecord
   belongs_to :user
   has_many :bookings
+  has_one_attached :photo
   validates :address, presence: true
   validates :name, presence: true
   validates :price, presence: true
