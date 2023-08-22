df09d2574b4ace71b845e7e68eddb922d79fcf58
Iwan Pavord <ipavord@gmail.com>
2023-08-22 16:15:07 +0100

    completed booking new

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,5 +5,21 @@ class BookingsController < ApplicationController
   end
 
   def create
+    @boat = Boat.find(params[:boat_id])
+    @user = current_user
+    @booking = Booking.new(booking_params)
+    @booking.user = @user
+    @booking.boat = @boat
+    if @booking.save
+      redirect_to(user_path(@user))
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:booking_date, :enddate)
   end
 end
