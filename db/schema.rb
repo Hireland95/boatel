d60475eeb69a83113f95040a88ff425be23f77f2
Pulkit Sehgal <pulkit.sehgal16@gmail.com>
2023-08-23 00:57:56 +0100

    added stuff, idk its 1am

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_22_144825) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_231803) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_22_144825) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.date "enddate"
+    t.text "message"
     t.index ["boat_id"], name: "index_bookings_on_boat_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
