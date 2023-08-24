25977b92cd70d9832615421890d00c23230fe312
Iwan Pavord <ipavord@gmail.com>
2023-08-24 17:19:46 +0100

    added photos validation

--- app/models/boat.rb
+++ app/models/boat.rb
@@ -5,6 +5,8 @@ class Boat < ApplicationRecord
   validates :address, presence: true
   validates :name, presence: true
   validates :price, presence: true
+  validates :boat_type, presence: true
+  validates :photos, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
 end
